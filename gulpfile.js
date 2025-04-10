a6befd369c8927689cdfc1b116143d404ba4ed40
Takuya0713 <shu.ydb21@gmail.com>
2025-04-11 07:24:19 +0900

    0411gulp
    gulpfiles.jsでsource.mapが表示されるように変更を実施。

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass")(require("sass"));
 var autoprefixer = require("autoprefixer");
+var sourcemaps = require("gulp-sourcemaps"); //0411
 var postcss = require("gulp-postcss");
 // Sassのタスクを設定し「{}」の中にSassコンパイルに必要な処理を記述
 gulp.task("sass", function () {
@@ -10,9 +11,11 @@ gulp.task("sass", function () {
   return (
     gulp
       .src("./scss/**/*.scss")
+      .pipe(sourcemaps.init()) // 0411sourcemapsの初期化
       //コンパイル時のアウトプットスタイルを「expanded」に指定
       .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
       .pipe(postcss([autoprefixer()])) // コンパイル先のディレクトリを指定
+      .pipe(sourcemaps.write("./")) // 0411 .mapファイルを出力（CSSと同じ場所）
       .pipe(gulp.dest("./css"))
   );
 });
