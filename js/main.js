09706a35527355564159c3b6b6e4a3deed2d9252
Takuya0713 <shu.ydb21@gmail.com>
2025-04-26 17:32:03 +0900

    0426
    ブログ部分を着手中

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@
 // アコーディオンメニュー部分
 $('.p-faq__accordion-question').click(function() {
   $(this).toggleClass('active');
+  // $('.p-faq__accordion-answer').toggleClass('active');
   $(this).next().slideToggle();
   });
