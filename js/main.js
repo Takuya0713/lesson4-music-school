47b6309d5f1e05bd4c34e1034e420c14af8b79f4
Takuya0713 <shu.ydb21@gmail.com>
2025-04-27 18:32:39 +0900

    0427
    各種修正。
    スライド機能は実装できず。

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 // AOS.init();
 // アコーディオンメニュー部分
-$('.p-faq__accordion-question').click(function() {
-  $(this).toggleClass('active');
-  // $('.p-faq__accordion-answer').toggleClass('active');
+$(".p-faq__accordion-question").click(function () {
+  $(this).toggleClass("active");
   $(this).next().slideToggle();
-  });
+});
+// 「生徒さんたちの声」スライド部分
+// $(document).ready(function () {
+//   $(".p-voice__wrapper").slick({
+//     rtl: true, // これで右から左にスライド
+//     autoplay: true, // 自動再生する場合
+//     autoplaySpeed: 3000, // スライドの間隔（ミリ秒）
+//     dots: true, // ドットナビゲーションを表示
+//     arrows: true, // 矢印を表示
+//   });
+// });
+
+// $(function () {
+//   // 【スライダー】
+//   $(".p-voice__wrapper").slick({
+//     // この中にパラメータを記載
+//     autoplay: true, // 自動再生
+//     autoplaySpeed: 4000, // 再生速度（ミリ秒設定） 1000ミリ秒=1秒
+//     speed: 1000,
+//     infinite: true, // 無限スライド
+//     arrows: false, // 矢印
+//   });
+// });
