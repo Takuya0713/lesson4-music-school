1e38d886ff89958c1d06ac4ee9ee3e2078e3a494
Takuya0713 <shu.ydb21@gmail.com>
2025-04-29 14:50:29 +0900

    0429

--- js/main.js
+++ js/main.js
@@ -5,24 +5,12 @@ $(".p-faq__accordion-question").click(function () {
   $(this).next().slideToggle();
 });
 // 「生徒さんたちの声」スライド部分
-// $(document).ready(function () {
-//   $(".p-voice__wrapper").slick({
-//     rtl: true, // これで右から左にスライド
-//     autoplay: true, // 自動再生する場合
-//     autoplaySpeed: 3000, // スライドの間隔（ミリ秒）
-//     dots: true, // ドットナビゲーションを表示
-//     arrows: true, // 矢印を表示
-//   });
-// });
-
-// $(function () {
-//   // 【スライダー】
-//   $(".p-voice__wrapper").slick({
-//     // この中にパラメータを記載
-//     autoplay: true, // 自動再生
-//     autoplaySpeed: 4000, // 再生速度（ミリ秒設定） 1000ミリ秒=1秒
-//     speed: 1000,
-//     infinite: true, // 無限スライド
-//     arrows: false, // 矢印
-//   });
-// });
+$(function () {
+  $(".p-voice__wrapper").slick({
+    autoplay: true, // 自動再生
+    autoplaySpeed: 4000, // 再生速度（ミリ秒設定） 1000ミリ秒=1秒
+    speed: 1000,
+    infinite: true, // 無限スライド
+    arrows: false, // 矢印
+  });
+});
